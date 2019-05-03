4da2e4398fe8d1e41470bf077bf1b18c5f2c81f8
frosteryd <linus.frosteryd@gmail.com>
2019-05-03 16:37:14 +0200

    make it possible to use ref

--- src/index.js
+++ src/index.js
@@ -1,112 +1,115 @@
-import React, { Component } from 'react';
-import Arrows from './Arrows.js';
-import './Slideshow.css';
+import React, { Component } from 'react'
+import Arrows from './Arrows.js'
+import './Slideshow.css'
 
 class Slideshow extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       currentSlide: props.defaultIndex,
       slideInterval: props.slideInterval,
       showIndex: props.showIndex,
       useDotIndex: props.useDotIndex,
+      useRef: props.useRef,
       showArrows: props.showArrows,
       effect: props.effect,
       autoplay: props.autoplay,
       enableKeyboard: props.enableKeyboard,
       slides: props.slides.length > 0 ? props.slides : props.children
-    };
+    }
 
-    this.runSlideShow = this.runSlideShow.bind(this);
-    this.autoSlideshow = this.autoSlideshow.bind(this);
-    this.restartSlideshow = this.restartSlideshow.bind(this);
-    this.increaseCount = this.increaseCount.bind(this);
-    this.decreaseCount = this.decreaseCount.bind(this);
-    this.handleKeyboard = this.handleKeyboard.bind(this);
+    this.runSlideShow = this.runSlideShow.bind(this)
+    this.autoSlideshow = this.autoSlideshow.bind(this)
+    this.restartSlideshow = this.restartSlideshow.bind(this)
+    this.increaseCount = this.increaseCount.bind(this)
+    this.decreaseCount = this.decreaseCount.bind(this)
+    this.handleKeyboard = this.handleKeyboard.bind(this)
   }
 
-  componentDidMount() {
-    if (this.state.autoplay) this.runSlideShow();
+  componentDidMount () {
+    if (this.state.autoplay) this.runSlideShow()
 
-    if (this.state.enableKeyboard)
-      document.addEventListener('keydown', this.handleKeyboard);
+    if (this.state.enableKeyboard) { document.addEventListener('keydown', this.handleKeyboard) }
+
+    if (this.state.useRef) { this.props.onRef(this) }
   }
 
-  handleKeyboard(e) {
+  handleKeyboard (e) {
     e.keyCode === 37
       ? this.decreaseCount()
-      : e.keyCode === 39 ? this.increaseCount() : null;
+      : e.keyCode === 39 ? this.increaseCount() : null
   }
 
-  runSlideShow() {
-    let intervalId = setInterval(this.autoSlideshow, this.state.slideInterval);
+  runSlideShow () {
+    let intervalId = setInterval(this.autoSlideshow, this.state.slideInterval)
     this.setState({
       intervalId
-    });
+    })
   }
 
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId);
-    document.removeEventListener('keydown', this.handleKeyboard);
+  componentWillUnmount () {
+    this.props.onRef(undefined)
+    clearInterval(this.state.intervalId)
+    document.removeEventListener('keydown', this.handleKeyboard)
   }
 
-  autoSlideshow() {
+  autoSlideshow () {
     this.setState({
       currentSlide: (this.state.currentSlide + 1) % this.state.slides.length
-    });
+    })
   }
 
-  restartSlideshow() {
-    clearInterval(this.state.intervalId);
-    this.runSlideShow();
+  restartSlideshow () {
+    clearInterval(this.state.intervalId)
+    this.runSlideShow()
   }
 
-  increaseCount() {
+  increaseCount () {
     this.state.effect === 'left'
       ? this.setState({
-          effect: 'right'
-        })
+        effect: 'right'
+      })
       : this.state.effect === 'bounce-left'
         ? this.setState({
-            effect: 'bounce-right'
-          })
-        : null;
+          effect: 'bounce-right'
+        })
+        : null
 
-    this.state.autoplay ? this.restartSlideshow() : null;
+    this.state.autoplay ? this.restartSlideshow() : null
     this.setState({
       currentSlide: (this.state.currentSlide + 1) % this.state.slides.length
-    });
+    })
   }
 
-  decreaseCount() {
+  decreaseCount () {
     this.state.effect === 'right'
       ? this.setState({
-          effect: 'left'
-        })
+        effect: 'left'
+      })
       : this.state.effect === 'bounce-right'
         ? this.setState({
-            effect: 'bounce-left'
-          })
-        : null;
+          effect: 'bounce-left'
+        })
+        : null
 
-    this.state.autoplay ? this.restartSlideshow() : null;
+    this.state.autoplay ? this.restartSlideshow() : null
 
-    let currentSlide;
+    let currentSlide
     currentSlide =
       this.state.currentSlide === 0
         ? this.state.slides.length - 1
-        : (currentSlide = this.state.currentSlide - 1);
+        : (currentSlide = this.state.currentSlide - 1)
     this.setState({
       currentSlide
-    });
+    })
   }
 
-  render() {
-    const { slides, showIndex, useDotIndex, effect, showArrows } = this.state;
+  render () {
+    const { slides, showIndex, useDotIndex, effect, showArrows } = this.state
 
-    let slideEffect = effect === undefined ? 'fade' : effect;
-    let slideShowSlides;
-    let slideShowIndex;
+    let slideEffect = effect === undefined ? 'fade' : effect
+    let slideShowSlides
+    let slideShowIndex
 
     if (!this.props.children) {
       slideShowSlides = slides.map((slide, i) => {
@@ -118,8 +121,8 @@ class Slideshow extends Component {
             key={i}
             style={{ backgroundImage: `url(${slide})` }}
           />
-        );
-      });
+        )
+      })
     } else {
       slideShowSlides = slides.map((slide, i) => {
         return (
@@ -131,13 +134,13 @@ class Slideshow extends Component {
           >
             {slide}
           </li>
-        );
-      });
+        )
+      })
     }
 
     if (useDotIndex) {
       slideShowIndex = (
-        <div className="show-index is-dot">
+        <div className='show-index is-dot'>
           {slides.map((slide, i) => {
             return (
               <span
@@ -146,16 +149,16 @@ class Slideshow extends Component {
                 }`}
                 key={`dot${i}`}
               />
-            );
+            )
           })}
         </div>
-      );
+      )
     } else {
       slideShowIndex = (
-        <div className="show-index is-text">
+        <div className='show-index is-text'>
           <p>{`${this.state.currentSlide + 1} / ${slides.length}`}</p>
         </div>
-      );
+      )
     }
 
     return (
@@ -166,8 +169,8 @@ class Slideshow extends Component {
           width: this.props.width || '100%'
         }}
       >
-        <div className="slideshow-container">
-          <ul className="slides">{slideShowSlides}</ul>
+        <div className='slideshow-container'>
+          <ul className='slides'>{slideShowSlides}</ul>
 
           {showArrows && (
             <Arrows
@@ -179,7 +182,7 @@ class Slideshow extends Component {
           {showIndex && slideShowIndex}
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -189,12 +192,13 @@ Slideshow.defaultProps = {
   autoplay: true,
   enableKeyboard: true,
   useDotIndex: false,
+  useRef: false,
   slideInterval: 2000,
   defaultIndex: 0,
   effect: 'fade',
   slides: [],
   height: '100%',
   width: '100%'
-};
+}
 
-export default Slideshow;
+export default Slideshow
